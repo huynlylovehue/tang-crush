b6aafd42c2fd1381081808a06df3d674cd762fca
huynlylovehue <108979976+huynlylovehue@users.noreply.github.com>
2022-07-09 18:08:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'My love à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao HL cứ đi nhầm đường
+    Lạc vào tim An lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'love HA',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: ' Tình cảm rung động với HL <33',
+    btnNo: 'hong be oi :3',
+    question:' Bạn có yêu mình hong mà mình mới xa đã nhớ rồi <3',
+    btnReply: 'Gửi cho gud gơ <3',
+    reply: ' Trên thích dưới yêu thoi <33333333',
+    mess: ' À the a mình cũng yêu bạn sương sương ne  😘😘',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
